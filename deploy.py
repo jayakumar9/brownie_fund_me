2ce04a937a42017f5353fee20183850a5a0b50d9
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 08:23:57 +0530

    Update deploy.py
    save

--- deploy.py
+++ deploy.py
@@ -6,6 +6,7 @@ from scripts.helpful_scripts import get_account
 
 def deploy_fund_me():
   account=get_account()
+  # pass the price feed address to our fundme contract
   fund_me=FundMe.deploy({"from":account},publish_source=True)
   print(f"Contract deployed to {fund_me.address}"
 
