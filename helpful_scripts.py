6171a66168d02e82f644ae8adc261068bbc5b242
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 07:22:13 +0530

    Update helpful_scripts.py
    save

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -1,2 +1,9 @@
 # Create this file under brownie_fund_me/scripts
 # refer video 2.solidity time 0:45:23
+from brownie import network, config, accounts
+
+def get_account():
+  if network.show_active()=="development":
+    return accounts[0]
+  else:
+    return accounts.add(config["wallets"]["from_key"])
