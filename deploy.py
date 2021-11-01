d928b768c01fb2a7775ad7359a01e94ef446e52e
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 07:11:37 +0530

    Create deploy.py
    save

---
+++ deploy.py
@@ -0,0 +1,12 @@
+# create this file under brownie_fund_me/scripts
+#refer vedio 2.solidity time 0:44:22
+from brownie import FundMe
+
+
+def deploy_fund_me():
+  account=get_account()
+
+
+def main():
+  deploy_fund_me()
+  
