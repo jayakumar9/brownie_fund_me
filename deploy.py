1775b4ef9de02f932b3f936b40383e3f399c5842
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 07:48:02 +0530

    Update deploy.py
    save

--- deploy.py
+++ deploy.py
@@ -1,10 +1,13 @@
 # create this file under brownie_fund_me/scripts
 #refer vedio 2.solidity time 0:44:22
 from brownie import FundMe
+from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
   account=get_account()
+  fund_me=FundMe.deploy({"from":account})
+  print(f"Contract deployed to {fund_me.address}"
 
 
 def main():
