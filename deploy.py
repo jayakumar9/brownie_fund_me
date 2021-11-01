b89bbaf984b298b8ea95b9402b13efe3b804f3e0
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 08:33:16 +0530

    Update deploy.py
    save

--- deploy.py
+++ deploy.py
@@ -7,7 +7,12 @@ from scripts.helpful_scripts import get_account
 def deploy_fund_me():
   account=get_account()
   # pass the price feed address to our fundme contract
-  fund_me=FundMe.deploy({"from":account},publish_source=True)
+  
+  #if we are on a persistent network like rinkeby, use the asssociated address
+  # otherwise, deploy mocks
+  if network.show_active()!="development":
+    price_feed_address="0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"
+  fund_me=FundMe.deploy(price_feed_address,{"from":account}, publish_source=True)
   print(f"Contract deployed to {fund_me.address}"
 
 
